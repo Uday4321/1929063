1fbe3e7193cf67fc8bd995d15e3359bb158c9fe6
Uday321 <ylosalka@gmail.com>
2019-08-07 23:59:56 +0530

    Revert "Update maxof2.c"
    This reverts commit 54f534abc1d781e55b1f2bc55c9aa01b37ef2bac.

--- 16-07-19/maxof2.c
+++ 16-07-19/maxof2.c
@@ -4,7 +4,7 @@ void main()
         int a,b;
         printf("Enter First Number- ");
         scanf("%d",&a);
-        
+        printf("Enter Second Number- ");
         scanf("%d",&b);
         int x=(a>b ? a : b);
         printf("Maximum- %d\n",x);
